8d3739cf18fefbd47bb4f01a4a8d4b3462dec681
Ayush-Sharma-007 <ayush120410sharma@gmail.com>
2024-08-10 20:20:39 +0530

    creating a database and connecting it with the code

---
+++ connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://ayush1210:allsmall2004@mern.ddk0v.mongodb.net/mydatabase01?retryWrites=true&w=majority&appName=MERN"
+
+// connects to the database
+
+
+// asynchronous - returns a promise 
+mongoose.connect(url)
+.then((result) => {
+    console.log("Connected to database");
+    
+})
+.catch((err) => {
+    console.log(err);
+    
+});
