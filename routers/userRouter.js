3bb3a17810f7a8dbeed869c7b3b3af84c928eae0
Ayush-Sharma-007 <ayush120410sharma@gmail.com>
2024-08-11 20:15:04 +0530

    created models and connected it to database

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const Model = require('../models/userModel');
 
 const router = express.Router();
 
 router.post('/add', (req, res) => { 
     console.log(req.body);
-    res.send('Response from user add')
+    
+    new Model(req.body).save()
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        res.status(500).json(err);
+    });
 });
 
 // getall
